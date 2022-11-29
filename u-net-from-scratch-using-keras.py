ed9983e68f7038458e2622a1167a830de0e72796
Saiful Islam <islam.saiful03@outlook.com>
2022-11-29 23:10:49 +0600

    contraction path added

--- u-net-from-scratch-using-keras.py
+++ u-net-from-scratch-using-keras.py
@@ -4,9 +4,32 @@ IMG_WIDTH = 128
 IMG_HEIGHT = 128
 IMG_CHANNELS = 3
 
-# first layers
 input = tf.keras.layers.Input((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
 input = tf.keras.layers.Lambda(lamda x : x / 255)(input)
+
+# first layer
 conv1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(input)
 conv1 = tf.keras.layers.Dropout(0.1)(conv1)
+conv1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(conv1)
 pooling1 = tf.keras.layers.MaxPooling2D((2,2))(conv1)
+
+# contraction path
+conv2 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(pooling1)
+conv2 = tf.keras.layers.Dropout(0.1)(conv2)
+conv2 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(conv2)
+pooling2 = tf.keras.layers.MaxPooling2D((2,2))(conv2)
+
+conv3 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(pooling2)
+conv3 = tf.keras.layers.Dropout(0.1)(conv3)
+conv3 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(conv3)
+pooling3 = tf.keras.layers.MaxPooling2D((2,2))(conv3)
+
+conv4 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(pooling3)
+conv4 = tf.keras.layers.Dropout(0.1)(conv4)
+conv4 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(conv4)
+pooling4 = tf.keras.layers.MaxPooling2D((2,2))(conv4)
+
+conv5 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(pooling4)
+conv5 = tf.keras.layers.Dropout(0.1)(conv5)
+conv5 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(conv5)
+pooling5 = tf.keras.layers.MaxPooling2D((2,2))(conv5)
